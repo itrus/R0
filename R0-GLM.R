a66b8b6d04363c7e6b3e17f45df70147af4da171
itrus <itrus@users.noreply.github.com>
2019-08-18 18:01:59 -0600

    Update R0-GLM.R

--- R0-GLM.R
+++ R0-GLM.R
@@ -3,10 +3,10 @@ I<-c(11, 13, 16, 19, 18, 18, 15, 10, 6, 2)
 C<-c(2, 2, 3, 0, 0, 0, 0, 0, 0, 0)
 N<-c(22, 22, 23, 23, 23, 23, 23, 23, 23, 23)
 
-x<-glm(C/S~log(I/N), family=binomial(link=cloglog), weights=-log(I/N))
+x<-glm(C/S~log(I/N), family=quasibinomial(link=cloglog), weights=-log(I/N))
 error95<-qnorm(0.975)*x$deviance[1]/sqrt(length(N))
 error68<-qnorm(0.84)*x$deviance[1]/sqrt(length(N))
 
-exp(x$coefficient[1])
+print(exp(x$coefficient[1]))
 cat("CI95% [", exp(x$coefficient[1]-error95), "-", exp(x$coefficient[1]+error95), "]")
 cat("CI68% [", exp(x$coefficient[1]-error68), "-", exp(x$coefficient[1]+error68), "]")
